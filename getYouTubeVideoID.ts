4b261010b9c36dadcd0f3c01bfaa976000bc1206
Luisda <luisdavid.colina@gmail.com>
2022-03-28 12:00:35 -0500

    update

--- getYouTubeVideoID.ts
+++ getYouTubeVideoID.ts
@@ -1,14 +1,12 @@
 export const getYouTubeVideoID = (youtubeUrl: string | string[]) => {
-  let url = youtubeUrl
-  let ID: string | string[] = ''
-  url = String(url)
-    .replace(/(>|<)/gi, '')
+  const url = String(youtubeUrl)
+    .replace(/(>|<)/gi, "")
     .split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
   if (url[2] !== undefined) {
+    let ID: string | string[] = ""
     ID = url[2].split(/[^0-9a-z_\-]/i)
     ID = String(ID[0])
-  } else {
-    ID = url
+    return ID
   }
-  return ID
+  return url
 }
